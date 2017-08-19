ee496ce24a10bbc01d1d1a45aef6df1135724959
Skillz4Killz <admin@ezlgg.com>
2017-08-19 19:07:47 -0400

    updated a bunch of commands and added music bot features

--- commands/Tools/contact.js
+++ commands/Tools/contact.js
@@ -1,54 +1,17 @@
-exports.init = async (client) => {
-  const schema = client.settings.guilds.schema;
-  if (!schema.bugChannelId) {
-    await client.settings.guilds.add("bugChannelId", {
-      "type":"String",
-      "default":null,
-      "array":false,
-      "sql":"TEXT NOT NULL DEFAULT ','"
-    });
-  }
-  if (!schema.ideaChannelId) {
-    await client.settings.guilds.add("ideaChannelId", {
-      "type":"String",
-      "default":null,
-      "array":false,
-      "sql":"TEXT NOT NULL DEFAULT ','"
-    });
-  }
-  if (!schema.messageChannelId) {
-    await client.settings.guilds.add("titleURL", {
-      "type":"String",
-      "default":null,
-      "array":false,
-      "sql":"TEXT NOT NULL DEFAULT ','"
-    });
-  }
-  if (!schema.reportChannelId) {
-    await client.settings.guilds.add("reportChannelId", {
-      "type":"String",
-      "default":null,
-      "array":false,
-      "sql":"TEXT NOT NULL DEFAULT ','"
-    });
-  }
-};
-
+const config = require('../../config/config.json');
  exports.run = async (client, msg, [type, ...content]) => {
-   // Declares all necessary variables to use for the command.
-   const { bugChannelId, ideaChannelId, messageChannelId, reportChannelId } = msg.guild.settings;
   try {
     // Determines which type of contact and sends to the appropriate channel.
     if (type === "bug") {
-      client.channels.get(bugChannelId).send(`Bug Report From: ${msg.author} in ${msg.guild}\n\n ${content.toString().replace(/,/g, " ")}`);
+      client.channels.get(client.config.const.bugChannelId).send(`Bug Report From: ${msg.author} in ${msg.guild}\n\n ${content.toString().replace(/,/g, " ")}`);
     } else if (type === "idea") {
-      client.channels.get(ideaChannelId).send(`New Idea From: ${msg.author} in ${msg.guild}\n\n ${content.toString().replace(/,/g, " ")}`);
+      client.channels.get(client.config.const.ideaChannelId).send(`New Idea From: ${msg.author} in ${msg.guild}\n\n ${content.toString().replace(/,/g, " ")}`);
     } else {
-      client.channels.get(messageChannelId).send(`New Message From: ${msg.author} in ${msg.guild}\n\n ${content.toString().replace(/,/g, " ")}`);
+      client.channels.get(client.config.const.messageChannelId).send(`New Message From: ${msg.author} in ${msg.guild}\n\n ${content.toString().replace(/,/g, " ")}`);
     }
   } catch (e) {
     msg.reply("Some error occured with relaying a message to the developers. A report has been sent to the developers.");
-    client.channels.get(reportChannelId).send(`There was an error trying to relaying a message to the developers: ${e} in ${msg.channel} on ${msg.guild} by ${msg.author}`);
+    client.channels.get(client.config.const.reportChannelId).send(`There was an error trying to relaying a message to the developers: ${e} in ${msg.channel} on ${msg.guild} by ${msg.author}`);
   }
 };
 
--- commands/base.js
+++ commands/base.js
@@ -1,15 +1,44 @@
-exports.run = async (client, msg, [...args]) => {
+exports.run = async (client, msg) => {
   try {
+    // code here
+    const embed = new client.methods.Embed()
+      .setTitle('This is your title, it can hold 256 characters')
+      .setAuthor(client.user.username, client.user.avatarURL())
+      /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+      .setColor(0x00AE86)
+      .setDescription('This is the main body of text, it can hold 2048 characters.')
+      .setFooter('This is the footer text, it can hold 2048 characters', 'http://i.imgur.com/w1vhFSR.png')
+      .setImage('http://i.imgur.com/yVpymuV.png')
+      .setThumbnail('http://i.imgur.com/p2qNFag.png')
+      /*
+   * Takes a Date object, defaults to current date.
+   */
+      .setTimestamp()
+      .setURL('https://discord.js.org/#/docs/main/indev/class/RichEmbed')
+      .addField('This is a field title, it can hold 256 characters',
+        'This is a field value, it can hold 2048 characters.')
+      /*
+   * Inline fields may not display as inline if the thumbnail and/or image is too big.
+   */
+      .addField('Inline Field', 'They can also be inline.', true)
+      /*
+   * Blank field, useful to create some space.
+   */
+      .addBlankField(true)
+      .addField('Inline Field 3', 'You can have a maximum of 25 fields.', true);
 
+    msg.channel.send({ embed });
   } catch (e) {
     msg.reply('Some error occured with xxxxxx. A report has been sent to the developers.');
-    client.channels.get("341020497309597696").send(`There was an error trying to xxxxxx: ${e} in ${msg.channel} on ${msg.guild} by ${msg.author}`);
+    client.channels.get('341020497309597696').send(`There was an error trying to xxxxxx: ${e} in ${msg.channel} on ${msg.guild} by ${msg.author}`);
   }
 };
 
 exports.conf = {
   enabled: true,
-  runIn: ["text"],
+  runIn: ['text'],
   aliases: [],
   permLevel: 0,
   botPerms: [],
@@ -18,9 +47,9 @@ exports.conf = {
 };
 
 exports.help = {
-  name: "name",
-  description: "Command Description",
-  usage: "",
-  usageDelim: "",
-  extendedHelp: "",
+  name: 'name',
+  description: 'Command Description',
+  usage: '',
+  usageDelim: '',
+  extendedHelp: '',
 };
